522261eeb3f082c14f39b6b327e630997751f721
James H Kim <jameshughkim.git@live.com>
2009-12-29 14:05:51 -0800

    initial commit

---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, you~");
+            Console.ReadLine();
+        }
+    }
+}
